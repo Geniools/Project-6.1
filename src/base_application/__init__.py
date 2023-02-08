b33c064580fe3fdea515a627bc03cf0d9332b497
Geniools <30665942+Geniools@users.noreply.github.com>
2023-02-08 11:26:22 +0100

    Intial version of API. Connection between NoSQL and app done

--- src/base_application/__init__.py
+++ src/base_application/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
-from flask_mongoengine import MongoEngine
+
+from pymongo import MongoClient
 
 from local_settings import MONGO_DB_URI
 
 # Flask application
 app = Flask(__name__)
-app.config["MONGODB_HOST"] = MONGO_DB_URI
+# app.config["MONGODB_HOST"] = MONGO_DB_URI
 
+# MongoDB engine (used for models like an ORM)
+# db_engine = MongoEngine(app)
 # MongoDB connection
-db = MongoEngine(app)
+client = MongoClient(MONGO_DB_URI)
+db = client["SportsAccounting"]
+transactions_collection = db["Transactions"]
 
 # Import views (must be imported after app, so that the app is aware of the API/views)
 from src.base_application import views
--- src/base_application/models.py
+++ src/base_application/models.py
@@ -1,7 +1,7 @@
-from src.base_application import db
-
-
-# Models used in the NoSQL database - similar to an ORM
-class Transaction(db.Document):
-	def to_json(self):
-		pass
+# from src.base_application import db_engine
+#
+#
+# # Models used in the NoSQL database - similar to an ORM
+# class Transaction(db_engine.Document):
+# 	def to_json(self):
+# 		pass
