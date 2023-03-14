2522c07523c47a53a53ff0e0d28ae2b7a004ea8a
ArianAtapour <89789041+ArianAtapour@users.noreply.github.com>
2023-03-14 20:27:32 +0100

    Member add-on module registration
    Also written it inside the admin.

--- src/member_module/admin.py
+++ src/member_module/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Member
+from .models import LinkedTransaction
 
 # Register your models here.
+
+admin.site.register(Member)
+admin.site.register(LinkedTransaction)
--- src/member_module/models.py
+++ src/member_module/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    id = models.IntegerField(primary_key=True,max_length=20)
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+
+class LinkedTransaction(models.Model):
+    id = models.IntegerField(primary_key=True, max_length=20)
+    member_id=models.IntegerField(models.ForeignKey(Member, on_delete=models.DO_NOTHING),max_length=20)
+    transaction_bank_reference=models.CharField(max_length=30)
