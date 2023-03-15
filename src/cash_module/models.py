05dca10cfc25b0bd464307d04d7250b90a12a1b1
Stef0002 <stefani.margaritova@student.nhlstenden.com>
2023-03-15 12:28:06 +0100

    Created cash add-on model

--- src/cash_module/models.py
+++ src/cash_module/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Cash_Transaction(models.Model):
+    id = models.IntegerField(primary_key=True, null=False)
+    balance_details_id = models.IntegerField(models.ForeignKey("base_app.BalanceDetails", on_delete=models.DO_NOTHING))
+    source = models.CharField(max_length=50, null=False)
+    target = models.CharField(max_length=50, null=False)
