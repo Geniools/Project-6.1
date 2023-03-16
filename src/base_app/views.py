ce441f1da853635ce98439d3a7a61ba86f5cf210
Geniools <30665942+Geniools@users.noreply.github.com>
2023-03-16 11:23:13 +0100

    Minor chages to views.

--- src/base_app/views.py
+++ src/base_app/views.py
@@ -7,76 +7,76 @@ from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.contrib import messages
 
-from . import transactions_collection
 from base_app.forms import UploadMT940Form
 from base_app.utils import MT940DBParser
+from . import transactions_collection
 
 
 def index(request):
-	answer = {
-		"message": "SportsAccounting API",
-		"api":     {
-			"Retrieve all transactions":                            "/api/transaction/",
-			"Get the total number of transactions in the NoSQL DB": "/api/transaction/count",
-			"Upload a file":                                        "/api/transaction/upload",
-			"Retrieve a specific transaction":                      "/api/transaction/<transaction_id>",
-			"Search for a keyword":                                 "/api/transaction/search/<keyword>"
-		}
-	}
-	return JsonResponse(answer)
+    answer = {
+        "message": "SportsAccounting API",
+        "api": {
+            "Retrieve all transactions": "/api/transaction/",
+            "Get the total number of transactions in the NoSQL DB": "/api/transaction/count",
+            "Upload a file": "/api/transaction/upload",
+            "Retrieve a specific transaction": "/api/transaction/<transaction_id>",
+            "Search for a keyword": "/api/transaction/search/<keyword>"
+        }
+    }
+    return JsonResponse(answer)
 
 
 def get_transactions_count(request):
-	response = {
-		"transactionsCount": transactions_collection.count_documents({})
-	}
-	return JsonResponse(response)
+    response = {
+        "transactionsCount": transactions_collection.count_documents({})
+    }
+    return JsonResponse(response)
 
 
 def get_transactions(request):
-	all_transactions = []
-	
-	for transaction in transactions_collection.find():
-		# print(transaction)
-		all_transactions.append(transaction)
-	
-	return JsonResponse(json.loads(json_util.dumps(all_transactions)), safe=False)
+    all_transactions = []
+
+    for transaction in transactions_collection.find():
+        # print(transaction)
+        all_transactions.append(transaction)
+
+    return JsonResponse(json.loads(json_util.dumps(all_transactions)), safe=False)
 
 
 def get_transaction(request, transaction_id: str):
-	transaction = transactions_collection.find_one({"_id": ObjectId(transaction_id)})
-	
-	if not transaction:
-		return Http404("Transaction not found")
-	
-	return JsonResponse(json.loads(json_util.dumps(transaction)))
+    transaction = transactions_collection.find_one({"_id": ObjectId(transaction_id)})
+
+    if not transaction:
+        return Http404("Transaction not found")
+
+    return JsonResponse(json.loads(json_util.dumps(transaction)))
 
 
 def search_keyword(request, keyword: str):
-	# TODO: implement functionality to search for keyword within transactions
-	return HttpResponse(f"The keyword that this app must search for is: <b>{keyword}</b>")
+    # TODO: implement functionality to search for keyword within transactions
+    return HttpResponse(f"The keyword that this app must search for is: <b>{keyword}</b>")
 
 
 def upload_file(request):
-	if request.POST:
-		form = UploadMT940Form(request.POST, request.FILES)
-		if form.is_valid():
-			files = request.FILES.getlist("file")
-			for file in files:
-				# Checking if the file is not too big (more than 2.5 MB)
-				if not file.multiple_chunks():
-					handler = MT940DBParser(file)
-					# Save to the NoSQL database
-					handler.save_to_nosql_db(transactions_collection)
-					# Save to the SQL database
-					# handler.save_to_sql_db()
-					# Add a success message
-					messages.success(request, f"File \"{file.name}\" uploaded successfully.")
-				else:
-					# Add an error message if the file is too big
-					messages.error(request, "The file is too big.")
-	else:
-		# If GET method or any other method called, return an empty form
-		form = UploadMT940Form()
-	
-	return render(request, "base_app/upload_transaction.html", {"form": form})
+    if request.POST:
+        form = UploadMT940Form(request.POST, request.FILES)
+        if form.is_valid():
+            files = request.FILES.getlist("file")
+            for file in files:
+                # Checking if the file is not too big (more than 2.5 MB)
+                if not file.multiple_chunks():
+                    handler = MT940DBParser(file)
+                    # Save to the NoSQL database
+                    handler.save_to_nosql_db(transactions_collection)
+                    # Save to the SQL database
+                    # handler.save_to_sql_db()
+                    # Add a success message
+                    messages.success(request, f"File \"{file.name}\" uploaded successfully.")
+                else:
+                    # Add an error message if the file is too big
+                    messages.error(request, "The file is too big.")
+    else:
+        # If GET method or any other method called, return an empty form
+        form = UploadMT940Form()
+
+    return render(request, "base_app/upload_transaction.html", {"form": form})
