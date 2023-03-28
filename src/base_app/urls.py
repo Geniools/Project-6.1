6fb3881b77fd40d8b91a216f982e2d924dfd9086
ArianAtapour <arian.atapour@student.nhlstenden.com>
2023-03-28 11:04:44 +0200

    mighty P U S H of test api

--- src/base_app/urls.py
+++ src/base_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import jsonTest
+from rest_framework.schemas import get_schema_view
+from .views import TransactionSerializer
+from .models import Transaction, File
+
+serializer_class = TransactionSerializer
 
 urlpatterns = [
 	path('', views.index, name='index'),
@@ -10,5 +14,12 @@ urlpatterns = [
 	path('transaction/upload/', views.upload_file, name='transaction_upload'),
 	path('transaction/<str:transaction_id>/', views.get_transaction, name='transaction'),
 	path('transaction/search/<str:keyword>/', views.search_keyword, name='transaction_search_keyword'),
-	path("test/", jsonTest, name="test"),
+	#path("test/", jsonTest, name="test"),
+	path('schema', get_schema_view(
+		title="Transaction API",
+		description="API for transactions",
+		version="1.0.0",
+		patterns= Transaction.objects.filter()
+	), name='openapi-schema'),
+
 ]
